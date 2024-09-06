88b945aec04b0fe2df5f57fb7a8ddd36f5ea6e0b
Nana Osafo-Bosompem <nana.osafo.bosompem@gmail.com>
2024-09-06 20:08:14 +0200

    rmoved keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,33 +1,19 @@
 export const environment = {
   production: true,
-  googleMapsAPIKey: "AIzaSyC2V-j8HkCCRNRHkdBieVOjsG1uVnLZRZE",
-  firebaseAPIKey: "AIzaSyBH4afXZCInC8hj5FLNu2GiYfiKmYNwQkk",
+  googleMapsAPIKey: "",
+  firebaseAPIKey: "",
   // firebaseAPIKey: "AIzaSyC2V-j8HkCCRNRHkdBieVOjsG1uVnLZRZE",
-  firebaseFCMServerKey: "key=AAAAQjxp7kM:APA91bERAFoxpT8VhId_bcHH6IHH3J_G4DUgD_uS90aHPnmT_j0zpnpGMF7g44OxexUkHCWuSdHNz_DP-VGMOB3ldj-PhFSPhpvmCFncuHQZqlNUcuCm8M6rW26kEd0_QGuei4c5ndkV",
-  // firebase:{
-  //   apiKey: "AIzaSyC2V-j8HkCCRNRHkdBieVOjsG1uVnLZRZE",
-  //   authDomain: "callforride.firebaseapp.com",
-  //   databaseURL: "https://callforride.firebaseio.com",
-  //   projectId: "callforride",
-  //   storageBucket: "callforride.appspot.com",
-  //   messagingSenderId: "284481416771",
-  //   appId: "1:284481416771:web:d91677758d61121f512ef6"
-  // }
+  firebaseFCMServerKey: "",
+ 
 
   firebase: {
-    // apiKey: "AIzaSyCuy4TT9s6GbOWyBrFGsNtZY6mcUQKd4bo",
-    // authDomain: "call4ride-5b9c5.firebaseapp.com",
-    // projectId: "call4ride-5b9c5",
-    // storageBucket: "call4ride-5b9c5.appspot.com",
-    // messagingSenderId: "322540371990",
-    // appId: "1:322540371990:web:515377f41576789d043537",
-    // measurementId: "G-83YYYQ1DN3"
-    apiKey: "AIzaSyBH4afXZCInC8hj5FLNu2GiYfiKmYNwQkk",
-    authDomain: "callforride-5071e.firebaseapp.com",
-    projectId: "callforride-5071e",
-    storageBucket: "callforride-5071e.appspot.com",
-    messagingSenderId: "2475702449",
-    appId: "1:2475702449:web:7c17bb2411991308f0ca90",
-    measurementId: "G-HQSTWGHPMS"
+    
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   },
 };
